5a25a1d43a99fd7f568a60a57684af746958d859
Valber-Andrade <168591912+Valber-Andrade@users.noreply.github.com>
2025-01-14 23:06:40 +0000

    Desafio concluido e depurado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,8 +9,7 @@ int main() {
     //Chamando o nome do desafio
     printf("Desafio Super Trunfo - Países\n");
     //Adicionando as variaveis
-    char codigodacidade[5];
-    char nome[20];
+    char codigodacidade[5], nome[20];
     float populacao;
     float area;
     float pib;
@@ -19,11 +18,11 @@ int main() {
     // Cadastro das Cartas:
     // Pedir ao usuario para informar os dados da cidade, utilizando o printf para fazer a pergunta, e o scanf para armazenar a resposta.
 
-    printf("Qual o código da cidade?\n");
-    scanf("%s" , &codigodacidade);
-
     printf("Qual o nome da cidade?\n");
-    scanf("%s", &nome);
+    fgets(nome,20,stdin); //Alterado o formato scanf para fgets, para concluir o desafio com nome composto
+
+    printf("Qual o código da cidade?\n");
+    scanf("%s" , codigodacidade);
 
     printf("Quantos habitantes a cidade possui?\n");
     scanf("%f", &populacao);
@@ -42,11 +41,11 @@ int main() {
     // Reposicionando o printf com as informações de saida, que foram utilizando no commit com valores fixos.
     // Agora serão exibidos após o usuário adicionar os dados das cartas.
 
-    printf("O codigo da Cidade é: %s\n", codigodacidade);
-    printf("O nome da cidade é: %s\n", nome);
+    printf("O codigo da cidade é: %s \n", codigodacidade);
+    printf("O nome da cidade é: %s \n", nome);
     printf("A cidade possui: %.3f milhões de habitantes.\n", populacao);
     printf("A aréa da cidade é de %.1f Km².\n", area);
-    printf("O PIB da cidade é de: R$ %.2f \n", pib);
+    printf("O PIB da cidade é de: R$%.2f \n", pib);
     printf("Essa cidade possui: %d pontos turisticos.\n", pontosturisticos);
 
 
