dc6af55b78697f836ec5e69d71d9c1f14434474e
Valber-Andrade <168591912+Valber-Andrade@users.noreply.github.com>
2025-01-19 22:37:06 +0000

    Desafio do Super Trunfo concluido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -19,6 +19,7 @@ int main() {
     //Adicionando a variavel de densidade populacional e PIB per capita
     float DensidadePopulacional1, DensidadePopulacional2;
     float PibPerCapita1, PibPerCapita2;
+    float SuperPoder1, SuperPoder2;
     
     // Cadastro das Cartas:
     // Pedir ao usuario para informar os dados da cidade, utilizando o printf para fazer a pergunta, e o scanf para armazenar a resposta.
@@ -52,10 +53,15 @@ int main() {
     //Dividindo a população pela area da cidade para ter a densidade populacional de cada uma das cidades
     DensidadePopulacional1 = populacao1 / area1;
     DensidadePopulacional2 = populacao2 / area2;
+    
     //Dividindo o PIB pela população para obter o PIB per capita de cada uma das cidades
     PibPerCapita1 = pib1 / populacao1;
     PibPerCapita2 = pib2 / populacao2;
-    
+
+    //Calculando o super poder das duas cidades
+    SuperPoder1 = populacao1 + area1 + pib1 + pontosturisticos1 + DensidadePopulacional1 + PibPerCapita1;
+    SuperPoder2 = populacao2 + area2 + pib2 + pontosturisticos2 + DensidadePopulacional2 + PibPerCapita2;
+
     // Exibição dos Dados das Cartas:
     // Exibando os valores inseridos para cada atributo da cidade, um por linha.
     // Reposicionando o printf com as informações de saida, que foram utilizando no commit com valores fixos.
@@ -73,6 +79,8 @@ int main() {
     //Colocando o printf para que o terminal calcule a densidade populacional e também calcule o PIB per capita
     printf("A densidade populacional é de: %.2f milhões de habitantes \n", DensidadePopulacional1);
     printf("O PIB per capita dessa cidade é de: %.2f mil \n", PibPerCapita1);
+    //Colocando o valor do Super Poder
+    printf("O Super Poder da primeira cidade é de: %.2f \n", SuperPoder1);
 
     // Vizualização de dados da primeira cidade
     printf("**************Os dados da Segunda cidade são:******************\n");
@@ -86,7 +94,21 @@ int main() {
     //Colocando o printf para que o terminal calcule a densidade populacional e também calcule o PIB per capita
     printf("A densidade populacional é de: %.2f milhões de habitantes \n", DensidadePopulacional2);
     printf("O PIB per capita dessa cidade é de: %.2f mil \n", PibPerCapita2);
+    //Colocando o valor do Super Poder
+    printf("O Super Poder da segunda cidade é de: %.2f \n", SuperPoder2);
 
+    //Vou comparar os valores dos dados das cidades
+
+
+    printf("*********Comparando os atributos de cada cidade*********\n");
+    printf("O terminal respondera com 1 para verdadeiro e 0 para falso\n");
+    printf("Densidade populacional da primeira cidade  > Densidade populacional da primeira cidade: %d\n", DensidadePopulacional1 > DensidadePopulacional2);
+    printf("A população da primeira cidade < A população da segunda cidade: %d\n", populacao1 < populacao2);
+    printf("A area da primeira cidade < A area da segunda cidade: %d\n", area1 < area2);
+    printf("O PIB da primeira cidade > O PIB da segunda cidade: %d\n", pib1 > pib2);
+    printf("A primeira cidade tem menos pontos turisticos do que a primeira: %d\n", pontosturisticos1 < pontosturisticos2);
+    printf("o PIB per capita da primeira cidade > o PIB per capita da segunda cidade: %d\n", PibPerCapita1 > PibPerCapita2);
+    printf("O SuperPoder da primeira cidade > O SuperPoder da segunda cidade? %d\n", SuperPoder1 > SuperPoder2);
 
 
     return 0;
