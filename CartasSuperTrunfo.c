e986ab06c40a25a1542caf3e9735bdfa1e15f0cd
Valber-Andrade <168591912+Valber-Andrade@users.noreply.github.com>
2025-02-22 01:11:25 +0000

    Atualização Final do Desafio no Nivel Mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,12 +5,14 @@ int main() {
     char estado1, estado2;
     char codigodacidade1[5], codigodacidade2[5];
     char nome1[20], nome2[20];
-    unsigned int populacao1, populacao2;
+    unsigned long int populacao1, populacao2;
     float area1, area2;
     float pib1, pib2;
     int pontosturisticos1, pontosturisticos2;
     float DensidadePopulacional1, DensidadePopulacional2;
     float PibPerCapita1, PibPerCapita2;
+    float SuperPoder1, SuperPoder2;
+    int resultado;
 
     // Cadastro das Cartas:
     //Adicionando o printf para o perguntar os dados da carta ao usuário
@@ -33,9 +35,9 @@ int main() {
     scanf("%s", nome2);
 
     printf("Quantos habitantes a cidade %s possui?\n", nome1);
-    scanf("%u", &populacao1);
+    scanf("%lu", &populacao1);
     printf("Quantos habitantes a cidade %s possui?\n", nome2);
-    scanf("%u", &populacao2);
+    scanf("%lu", &populacao2);
 
     printf("Qual o tamanho da aréa da cidade de %s?\n", nome1);
     scanf("%f", &area1);
@@ -60,19 +62,25 @@ int main() {
     PibPerCapita1 = pib1 / populacao1;
     PibPerCapita2 = pib2 / populacao2;
 
+    //Calculando o super poder das duas cidades
+    SuperPoder1 = populacao1 + area1 + pib1 + pontosturisticos1 + DensidadePopulacional1 + PibPerCapita1;
+    SuperPoder2 = populacao2 + area2 + pib2 + pontosturisticos2 + DensidadePopulacional2 + PibPerCapita2;
+
     // Vizualização de dados da primeira cidade
     printf("\n**************Os dados da Primeira cidade são:******************\n");
     printf("Cidade 1:\n");
     printf("Letra do Estado: %c \n", estado1);
     printf("O codigo da cidade é: %s \n", codigodacidade1);
     printf("O nome da cidade é: %s\n", nome1);
-    printf("A cidade possui: %u milhões de habitantes.\n", populacao1);
+    printf("A cidade possui: %lu milhões de habitantes.\n", populacao1);
     printf("A aréa da cidade é de %.1f Km².\n", area1);
     printf("O PIB da cidade é de: R$%.2f bilhões\n", pib1);
     printf("Essa cidade possui: %d pontos turisticos.\n", pontosturisticos1);
     //Colocando o printf para que o terminal mostre o calculo da densidade populacional e também o calculo do PIB per capita
     printf("A densidade populacional é de: %.2f milhões de habitantes \n", DensidadePopulacional1);
     printf("O PIB per capita dessa cidade é de: R$%.2f milhões \n", PibPerCapita1);
+    //Exibindo o valor do Super Poder
+    printf("O Super Poder da primeira cidade é de: %.2f \n", SuperPoder1);
 
     // Vizualização de dados da segunda cidade
     printf("\n**************Os dados da Segunda cidade são:******************\n");
@@ -80,14 +88,29 @@ int main() {
     printf("Letra do Estado: %c \n", estado2);
     printf("O codigo da cidade é: %s \n", codigodacidade2);
     printf("O nome da cidade é: %s\n", nome2);
-    printf("A cidade possui: %.u milhões de habitantes.\n", populacao2);
+    printf("A cidade possui: %lu milhões de habitantes.\n", populacao2);
     printf("A aréa da cidade é de %.1f Km².\n", area2);
     printf("O PIB da cidade é de: R$%.2f bilhões\n", pib2);
     printf("Essa cidade possui: %d pontos turisticos.\n", pontosturisticos2);
     //Colocando o printf para que o terminal mostre o calculo da densidade populacional e também o calculo do PIB per capita
     printf("A densidade populacional é de: %.2f milhões de habitantes \n", DensidadePopulacional2);
     printf("O PIB per capita dessa cidade é de: R$%.2f milhões \n", PibPerCapita2);
+    //Exibindo o valor do Super Poder
+    printf("O Super Poder da segunda cidade é de: %.2f \n", SuperPoder2);
 
+    //Vou comparar os valores dos dados das cidades
+
+
+    printf("\n*********Comparando os atributos de cada cidade*********\n");
+    printf("O terminal respondera com 1 para verdadeiro e 0 para falso\n\n");
+
+    printf("Densidade populacional: %d\n", DensidadePopulacional1 < DensidadePopulacional2);
+    printf("A população: %d\n", populacao1 > populacao2);
+    printf("A area: %d\n", area1 > area2);
+    printf("O PIB: %d\n", pib1 > pib2);
+    printf("Pontos turisticos: %d\n", pontosturisticos1 > pontosturisticos2);
+    printf("o PIB per capita: %d\n", PibPerCapita1 > PibPerCapita2);
+    printf("O SuperPoder: %d\n", SuperPoder1 > SuperPoder2);
     
 
     return 0;
